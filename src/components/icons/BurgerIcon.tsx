1c6170de8a23352a8d85203ff23b909b2e31b231
Manas <manas.jadye@gmail.com>
2026-07-28 12:48:38 +1200

    Added React icons

---
+++ src/components/icons/BurgerIcon.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from "react";
+
+export const BurgerIcon: FC = () => (
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    aria-hidden="true"
+    focusable="false"
+  >
+    <path
+      d="M3 6h18M3 12h18M3 18h18"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+    />
+  </svg>
+);
